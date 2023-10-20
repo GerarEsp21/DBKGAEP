cf7ff81fbcca3cd7de46aafe0081395e1b9e23f9
Gerardo Alfredo Espinoza Portillo <gerardoaespinoza15@gmail.com>
2023-10-19 20:00:10 -0600

    Documentacion generada de Swagger 8.5
    Modificación de route y controladores

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API  de Gerardo Alfredo Espinoza Portillo",
+ *      description="API de ejemplo para la clase de Desarrollo de BackEnd y APIs",
+ *      @OA\Contact(
+ *          email="gerardoaespinoza15@gmail.com",
+ *          name="Gerardo Alfredo Espinoza Portillo",
+ *          url="https://localhost/api/"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
--- app/Http/Controllers/respuestaController.php
+++ app/Http/Controllers/respuestaController.php
@@ -8,6 +8,25 @@ class respuestaController extends Controller
 {
     
 
+/**
+ * 
+ * Descripción de prueba para la clase de Desarrollo de BackEnd y API desarrollada por todo el equipo el 19/10/2023
+ * @return \Illuminate\Http\Response
+ *
+ * @OA\Get(
+ *     path="/api/ejecutar",
+ *     tags={"ejecutar"},
+ *     summary="Este es un endpoint o metodo para obtener datos y mostrarlo al usuario",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Se devuelven todos los registros cuando la consulta sea fatisfactoria"
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="No se pudo realizar la ejecución al parecer hubo un error interno"
+ *     )
+ * ) 
+ */
 
 public function index(){
 
